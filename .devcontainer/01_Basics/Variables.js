10b7e353481ee9466f0bcf039acb34f2118d2625
Pankaj Solanki <156430478+student-pankaj@users.noreply.github.com>
2026-03-25 17:39:57 +0000

    notes

---
+++ .devcontainer/01_Basics/Variables.js
@@ -0,0 +1,12 @@
+const acoountId = 15489765
+let name = "pankaj"
+let email = "pnkj@gmail.com"
+let passWord = 5489525;
+var add = "Banguluru"
+
+name = "pankakj solanki"
+email = "Slnki@gmail.com"
+// prefer not to usevar because of issues in block scop and function scope.
+
+console.table([accountId, name, email,passWord,add])
+
--- .devcontainer/01_Basics/test.js
+++ .devcontainer/01_Basics/test.js
@@ -1,1 +1,6 @@
 console.log("pankaj solanki")
+
+
+
+
+
