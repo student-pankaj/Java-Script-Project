347ec80c04fb3425a851142166deec216be5948f
Pankaj Solanki <156430478+student-pankaj@users.noreply.github.com>
2026-03-25 19:42:25 +0000

    dataTypes

---
+++ .devcontainer/01_Basics/dataTypes.js
@@ -0,0 +1,30 @@
+"use strick"; // treat all js code as a new version
+
+ //alert(3+3) // we are using node.js , not a browser  thats why its show error 
+console.log("Pankaj")
+console.log(5+5)
+
+let name = "pankaj"
+let age = 23
+let isLoggedIn = false
+let sate;
+
+
+// Notes 
+
+// Number -> 2 to power 53
+// bigint -> for very large numbers
+//String -> " "
+// null -> stadealon value
+// boolean -> true / false
+// undifine -> not assign any value 
+// symbol -> unique
+
+// # Object 
+console.log(trypeof , undefined); // undefines
+console.log(typeof null); //objec
+
+
+
+
+
