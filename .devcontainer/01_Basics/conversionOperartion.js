e55b12785f9f8d3b0fbeec3bc65e35fa118f77e6
Pankaj Solanki <156430478+student-pankaj@users.noreply.github.com>
2026-03-26 10:52:29 +0000

    conversionOperation

---
+++ .devcontainer/01_Basics/conversionOperartion.js
@@ -0,0 +1,53 @@
+let n = 54;
+console.log(typeof n)
+
+let number  = String(n)
+console.log(number)
+
+let b = 1
+let booleans = Boolean(b)
+
+let nm = "Pankaj"
+
+let name = Number(nm)
+console.log(name)
+
+// "33" -> 33
+// "33abc" -> NAN
+// true -> 1, false -> 0
+// "" -> false
+// "pankaj" -> true
+let isLogged = "Pankaj"
+
+let blIsLogged = Boolean(isLogged)
+console.log(blIsLogged)
+
+//*************************************Operation*************************************************
+
+let val = 3
+let newval = -val
+console.log(newval)
+console.log(2+2)
+console.log(2-2)
+console.log(2*3)
+console.log(5/2)
+console.log(7*5-8*7)
+
+let str1 = "pankaj"
+let str2 = "Solanki"
+
+let ans = str1 + str2
+
+console.log(ans)
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2+5
+console.log(num2)
+
+let  count = 100
+let tcount = count++; // post increment 
+
+let no = 200
+let tno = ++no; // pre increment 
+
